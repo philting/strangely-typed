6e543b6184e95287a5637f404251ffeb4479d892
Philip Ting <philipjting@yahoo.com>
2022-01-03 00:25:02 +0100

    added prettier and eslint

--- class-methods/index.ts
+++ class-methods/index.ts
@@ -1,3 +1,20 @@
-class Jedi {
+export type Alignment = 'neutral' | 'light' | 'dark';
 
+export class Jedi {
+  private alignment: Alignment;
+  constructor() {
+    this.alignment = 'neutral';
+  }
+
+  public turnLight() {
+    this.alignment = 'light';
+  }
+
+  public turnDark = () => {
+    this.alignment = 'dark';
+  };
+
+  public getAlignment() {
+    return this.alignment;
+  }
 }
